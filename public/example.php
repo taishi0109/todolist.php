f3248e94915a2437b8575a7249d077ed78934181
yoshifumi0307 <nicchi0307@gmail.com>
2020-10-13 17:28:11 +0900

    Initialize repository

---
+++ public/example.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>example Test</title>
+ </head>
+ <body>
+ <?php echo '<p>example test</p>'; ?> 
+<p>yoshifumi nishimura</p> 
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+<p>西村</p> 
+</body>
+</html>
