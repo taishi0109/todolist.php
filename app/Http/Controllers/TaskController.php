488ec2076ead7e42548140ac0de132b134adad86
yoshifumi0307 <nicchi0307@gmail.com>
2020-10-14 12:38:12 +0900

    10-14-1

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TaskController extends Controllers
+class TaskController extends Controller
 {
     public function index()
     {
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/', 'TaskController@index')->name('tasks.index');
 
 Route::get('/archives/', function() {
     return '記事一覧';
@@ -8,4 +12,14 @@ Route::get('/archives/', function() {
 
 Route::get('/archives/{category}/', function($category) {
     return $category.'は変態';
+});
+
+Route::get('/beginner', 'BeginnerController@index');
+
+Route::get('/info', function () {
+    return 'Hello World';
+});
+
+Route::get('/info', function() {
+    return view('info');
 });
